290f453f2d7d6860f510e6125b727309768b0191
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-06 16:22:17 +0000

    Completed

--- umbrella.rb
+++ umbrella.rb
@@ -8,7 +8,7 @@ puts "Will you need an umbrella today?".center(line_width)
 puts "="*line_width
 puts
 p "Where are you located?"
-user_location = gets.chomp
+user_location = gets.chomp.capitalize
 
 # Getting Google Maps API Data
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=" + ENV.fetch("GMAPS_KEY")
@@ -59,6 +59,7 @@ current_temp = currently_hash.fetch("temperature")
 
 puts
 puts "It is currently #{current_temp}°F."
+puts
 
 ## Getting the next hour weather forecast
 
@@ -67,3 +68,30 @@ hourly_hash = pirate_parsed_response.fetch("hourly")
 hourly_summary = hourly_hash.fetch("summary")
 
 puts "For the next hour, the weather is #{hourly_summary}"
+
+hourly_data_array = hourly_hash.fetch("data")
+
+next_twelve_hours = hourly_data_array[1..12]
+
+precip_prob_threshold = 0.10
+precipitation = false
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability")
+  if precip_prob > precip_prob_threshold
+    precipitation = true
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+    
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if precipitation == true
+  puts "Looks like there's a forecast of rain today. You might want to take an umbrella!"
+else
+  puts "Looks like there's no forecast of rain today.You probably won't need an umbrella today."
+end
